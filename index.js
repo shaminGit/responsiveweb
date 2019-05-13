43de12e7bc18c424bfa5d4c3b6ca2d5b5c3ac50a
shaminGit <38139062+shaminGit@users.noreply.github.com>
2019-05-13 09:42:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var app = express();
-app.use(express.static('frontend'));    // to server static files to the client
+
+app.use(express.static(__dirname + '/frontend'));    // to server static files to the client
+app.set('port',(process.env.PORT || 4000));
+app.set('views', __dirname +'/views');
+app.set('view engine'  ,'ejs');
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'frontend/index.html'));
+    res.render('/index.html');
 });
-var server = app.listen(process.env.PORT || 4000, function(){              //start the server at port 5000
-  console.log('Server listening on port ' + server.address().port);
-});
+var server = app.listen(app.get('port'), function(){              //start the server at port 5000
+  console.log('Server listening on port ' + app.get('port'));
